07ebf3597f549e2121c75ea7486d79737f044cea
Stone Comstock <comstockstone@yahoo.com>
2023-03-08 20:59:27 +0700

    Basic pages created and routed in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateRecipe from './pages/CreateRecipe';
+import SavedRecipes from './pages/SavedRecipes';
+import Auth from './pages/Auth';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create-recipe' element={<CreateRecipe />} />
+          <Route path='/saved-recipes' element={<SavedRecipes />} />
+          <Route path='/auth' element={<Auth />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
