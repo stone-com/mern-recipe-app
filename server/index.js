cfee568b0fec86a65720c726dc7d275cbadc8080
Stone Comstock <comstockstone@yahoo.com>
2023-03-08 14:43:22 +0700

    connected to db and create user model

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI)
 app.use(express.json());
 app.use(cors());
 
