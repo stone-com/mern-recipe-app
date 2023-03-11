204a191b3e2acfc79b3b35cac7ee31b5e6224e6f
Stone Comstock <comstockstone@yahoo.com>
2023-03-11 15:54:43 +0700

    Route and controller to save recipe

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import { Recipe } from '../models/Recipes.js';
+import { User } from '../models/Users.js';
 
 export const getRecipes = asyncHandler(async (req, res) => {
   try {
@@ -15,6 +16,18 @@ export const createRecipe = asyncHandler(async (req, res) => {
   try {
     const response = await recipe.save();
     res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+export const saveRecipe = asyncHandler(async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.body.recipeId);
+    const user = await User.findById(req.body.userId);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
   } catch (error) {
     res.json(error);
   }
--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { Recipe } from '../models/Recipes.js';
-import { getRecipes, createRecipe } from '../controllers/recipeController.js';
+import {
+  getRecipes,
+  createRecipe,
+  saveRecipe,
+} from '../controllers/recipeController.js';
 
 const router = express.Router();
 
@@ -9,5 +13,7 @@ const router = express.Router();
 router.get('/', getRecipes);
 
 router.post('/', createRecipe);
+
+router.put('/', saveRecipe);
 
 export { router as recipeRouter };
